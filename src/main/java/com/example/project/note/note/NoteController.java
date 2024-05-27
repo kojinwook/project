7c37b9ddb3bf13d8df5ab83f7c58b59917499362
kojinwook <gojinug4@gmail.com>
2024-05-27 09:41:37 +0900

    1

--- src/main/java/com/example/project/note/note/NoteController.java
+++ src/main/java/com/example/project/note/note/NoteController.java
@@ -54,16 +54,35 @@ public class NoteController {
     @PostMapping("/update")
     public String update(Long id, String title, String content) {
         Note note = noteRepository.findById(id).get();
+        if(title.trim().length()==0){
+            title="마감된 매치";
+        }
         note.setTitle(title);
         note.setContent(content);
 
         noteRepository.save(note);
         return "redirect:/detail/" + id;
     }
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Long id){
+        noteRepository.deleteById(id);
+        return "redirect:/";
+    }
     private Note saveDefault(){
         Note note = new Note();
-        note.setTitle("new title..");
-        note.setContent("");
+        note.setTitle("새 매치");
+        note.setContent("* 시간 : \n " +
+                "* 장소 : \n +" +
+                "* 모든 레벨 \n " +
+                "* 금액 / 시간  : 인당 10000원 / 2시간 \n" +
+                "* 인원수 : 10 ~ 18 명 \n" +
+                "* 6 vs 6 3파전 \n" +
+                "* 풋살화 / 운동화 착용 \n" +
+                "* 주의 사항 :  \n" +
+                "* 구장 정보 : \n" +
+                "* 매치 진행 방식 : \n" +
+                "* 매치 규칙 : \n" +
+                "환불 정책 : \n);");
         note.setCreateDate(LocalDateTime.now());
 
         noteRepository.save(note);
